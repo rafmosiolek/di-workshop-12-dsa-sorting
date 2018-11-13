ffaca1064124e88f549f5624450aa99b7c7a5d44
RafM <rkjmosiolek@gmail.com>
2018-11-13 11:47:41 +0000

    create an array (length 100) of random values between 0 and 900; sort it, meassure the performance via nodejs performance hook

--- bubbleSortAlgo.js
+++ bubbleSortAlgo.js
@@ -1,3 +1,5 @@
+const { performance } = require('perf_hooks')
+
 function bubbleSort(a) {
     var swapped;
     do {
@@ -13,7 +15,11 @@ function bubbleSort(a) {
     } while (swapped);
 }
  
-var ary = [34, 203, 3, 746, 200, 984, 198, 764, 9];
+// var ary = [34, 203, 3, 746, 200, 984, 198, 764, 9];
+var ary = Array.from({length: 100}, () => Math.floor(Math.random() * 900));
 
+var t0 = performance.now();
 bubbleSort(ary);
+var t1 = performance.now();
 console.log(ary);
+console.log("Call to bubbleSort(ary) took " + (t1 - t0) + " milliseconds.")
